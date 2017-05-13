47783a4200c0af4d6d98ef5c7ddedaec97b96c94
Per Eriksson <pere5@kth.se>
2017-05-13 11:18:59 +0200

    Some http with serverside REST

--- AngularTypeScript/src/main/java/integrator/controllers/GetController.java
+++ AngularTypeScript/src/main/java/integrator/controllers/GetController.java
@@ -11,32 +11,45 @@ import java.util.Map;
 @RestController
 public class GetController {
 
-    @GetMapping(value="/api/heroes")
-    public List<Map<String, Object>> getCategorySuggestions() {
-        List<Map<String, Object>> heroList = new ArrayList<>();
+    private static List<Map<String, Object>> HERO_LIST = new ArrayList<>();
+
+    static {
         for (int i = 0; i < 10; i++) {
-            heroList.add(new HashMap<>());
+            HERO_LIST.add(new HashMap<>());
         }
-        heroList.get(0).put("id", 11);
-        heroList.get(0).put("name", "Mr. Nice");
-        heroList.get(1).put("id", 12);
-        heroList.get(1).put("name", "Narco");
-        heroList.get(2).put("id", 13);
-        heroList.get(2).put("name", "Bombaso");
-        heroList.get(3).put("id", 14);
-        heroList.get(3).put("name", "Celeritas");
-        heroList.get(4).put("id", 15);
-        heroList.get(4).put("name", "Magenta");
-        heroList.get(5).put("id", 16);
-        heroList.get(5).put("name", "Rubberman");
-        heroList.get(6).put("id", 17);
-        heroList.get(6).put("name", "Dynama");
-        heroList.get(7).put("id", 18);
-        heroList.get(7).put("name", "Dr IQ");
-        heroList.get(8).put("id", 19);
-        heroList.get(8).put("name", "Magma");
-        heroList.get(9).put("id", 20);
-        heroList.get(9).put("name", "Tornado");
-        return heroList;
+        HERO_LIST.get(0).put("id", 11);
+        HERO_LIST.get(0).put("name", "Mr. Nice");
+        HERO_LIST.get(1).put("id", 12);
+        HERO_LIST.get(1).put("name", "Narco");
+        HERO_LIST.get(2).put("id", 13);
+        HERO_LIST.get(2).put("name", "Bombaso");
+        HERO_LIST.get(3).put("id", 14);
+        HERO_LIST.get(3).put("name", "Celeritas");
+        HERO_LIST.get(4).put("id", 15);
+        HERO_LIST.get(4).put("name", "Magenta");
+        HERO_LIST.get(5).put("id", 16);
+        HERO_LIST.get(5).put("name", "Rubberman");
+        HERO_LIST.get(6).put("id", 17);
+        HERO_LIST.get(6).put("name", "Dynama");
+        HERO_LIST.get(7).put("id", 18);
+        HERO_LIST.get(7).put("name", "Dr IQ");
+        HERO_LIST.get(8).put("id", 19);
+        HERO_LIST.get(8).put("name", "Magma");
+        HERO_LIST.get(9).put("id", 20);
+        HERO_LIST.get(9).put("name", "Tornado");
+    }
+    @GetMapping(value="/api/heroes")
+    public List<Map<String, Object>> getHeroes() {
+        return HERO_LIST;
+    }
+
+    @GetMapping(value="/api/heroes/{id}")
+    public Map<String, Object> getHeroById(@PathVariable("id") int heroId) {
+        for (Map<String, Object> hero: HERO_LIST) {
+            if ((int)hero.get("id") == heroId) {
+                return hero;
+            }
+        }
+        return null;
     }
 }
--- my-app/src/app/hero.service.ts
+++ my-app/src/app/hero.service.ts
@@ -1,24 +1,32 @@
-import { Injectable } from '@angular/core';
-
-import {Hero} from "./hero";
-import { HEROES } from './mock-heroes';
+import { Injectable }    from '@angular/core';
+import { Headers, Http } from '@angular/http';
+import 'rxjs/add/operator/toPromise';
+import { Hero } from './hero';
 
 @Injectable()
 export class HeroService {
 
-  getHeroes(): Promise<Hero[]> {
-    return Promise.resolve(HEROES);
-  }
+  private heroesUrl = '/api/heroes';  // URL to web api
 
-  getHeroesSlowly(): Promise<Hero[]> {
-    return new Promise(resolve => {
-      // Simulate server latency with 2 second delay
-      setTimeout(() => resolve(this.getHeroes()), 700);
-    });
+  constructor(private http: Http) { }
+
+  getHeroes(): Promise<Hero[]> {
+    return this.http.get(this.heroesUrl)
+      .toPromise()
+      .then(response => response.json() as Hero[])
+      .catch(this.handleError);
   }
 
   getHero(id: number): Promise<Hero> {
-    return this.getHeroes()
-      .then(heroes => heroes.find(hero => hero.id === id));
+    const url = `${this.heroesUrl}/${id}`;
+    return this.http.get(url)
+      .toPromise()
+      .then(response => response.json() as Hero)
+      .catch(this.handleError);
+  }
+
+  private handleError(error: any): Promise<any> {
+    console.error('An error occurred', error); // for demo purposes only
+    return Promise.reject(error.message || error);
   }
 }
